995e602b9e676d0429538dccaa118b8c9f296da9
Sreehari vr <sreeharivr855@gmail.com>
2022-12-11 13:35:34 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput,ToastAndroid} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
 
 export default function App() {
+  const [task,setTask] = useState(null)
+
+  const add = async()=>{
+    try{
+      console.log(task)
+      if(task != null){
+        await AsyncStorage.setItem('note',task)
+        ToastAndroid.show('Task added', ToastAndroid.SHORT);
+       
+      }
+    }catch(e){
+      console.error(e)
+    }
+  }
+
+  const get = async()=>{
+    try{
+      const value = await AsyncStorage.getItem('note')
+      if(value != null){
+        setTask(value)
+      }
+    }catch(e){
+      console.error(e)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>{task}</Text>
+      <TextInput onChangeText={e=>setTask(e)} style = {styles.input} placeholder = "Enter task" />
+      <Separator />
+      <Button onPress={add} title='Add' />
+      <Separator />
+      <Button onPress={get} title='Get' />
     </View>
   );
 }
@@ -18,4 +52,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    width:'80%',
+    height:50,
+    borderWidth:1,
+    borderColor:'#00000',
+    margin:10,
+    paddingLeft:20,
+    borderRadius:5,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  title:{
+    fontSize:20,
+    fontWeight:'bold',
+  }
 });
